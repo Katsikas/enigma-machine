f7835127f98f9dfc5aa3711b02f8e92852df05b0
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-07-30 17:00:01 +0300

    feat: add simple Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <nav className="header-nav">
+        <div className="header-logo">
+          <img src="rotor.png" alt="An enigma rotor" height={80} width={80} />
+        </div>
+      </nav>
+    </header>
+  );
+}
